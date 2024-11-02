f48923a45fba9c3f6242586b24f3098a44ea788f
Geoff Turk <geoff@ic3.dev>
2024-11-02 22:42:54 +0100

    chore: add basic error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { notFound, onError } from "stoker/middlewares";
 
 const app = new OpenAPIHono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.notFound(notFound);
+app.onError(onError);
 
 export default app;
