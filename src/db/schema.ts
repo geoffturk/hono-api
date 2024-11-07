d7ba946a4286b264f5d19812b2ff71bcfcd2bd95
Geoff Turk <geoff@ic3.dev>
2024-11-07 17:40:45 +0100

    chore: setup drizzle-zod

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { createSelectSchema } from "drizzle-zod";
 
 export const tasks = sqliteTable("tasks", {
   id: integer({ mode: "number" })
@@ -14,3 +15,5 @@ export const tasks = sqliteTable("tasks", {
     .$defaultFn(() => new Date())
     .$onUpdate(() => new Date()),
 });
+
+export const selectTasksSchema = createSelectSchema(tasks);
--- src/routes/tasks/tasks.routes.ts
+++ src/routes/tasks/tasks.routes.ts
@@ -2,16 +2,15 @@ import { createRoute, z } from "@hono/zod-openapi";
 import * as HttpStatusCodes from "stoker/http-status-codes";
 import { jsonContent } from "stoker/openapi/helpers";
 
+import { selectTasksSchema } from "@/db/schema.ts";
+
 export const list = createRoute({
   tags: ["tasks"],
   path: "/tasks",
   method: "get",
   responses: {
     [HttpStatusCodes.OK]: jsonContent(
-      z.array(z.object({
-        name: z.string(),
-        done: z.boolean(),
-      })),
+      z.array(selectTasksSchema),
       "Hono API tasks list description",
     ),
   },
